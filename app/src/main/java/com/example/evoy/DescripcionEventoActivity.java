b25537d0556627405bd84c81301133535f7c76ba
Javi <13javiergc@gmail.com>
2019-05-12 11:40:18 +0200

    Descripción Evento Activity

---
+++ app/src/main/java/com/example/evoy/DescripcionEventoActivity.java
@@ -0,0 +1,15 @@
+package com.example.evoy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DescripcionEventoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_descripcion_evento);
+
+
+    }
+}
